58993e908ffa32da606efe46ac2b2f6c65cfad58
yangxuemin <18295894749@163.com>
2024-01-13 17:23:10 +0800

    修改

--- ruoyi-model/src/main/java/com/ruoyi/shop/domain/ShopActivity.java
+++ ruoyi-model/src/main/java/com/ruoyi/shop/domain/ShopActivity.java
@@ -44,8 +44,8 @@ public class ShopActivity extends BaseEntity {
     /**
      * 活动开始时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "活动开始时间", width = 30, dateFormat = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Excel(name = "活动开始时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty("活动开始时间")
     private Date beginDate;
 
