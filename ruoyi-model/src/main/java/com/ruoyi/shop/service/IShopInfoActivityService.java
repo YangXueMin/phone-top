8b2e5b7f4cc2d80d2d6c56e995a8aad6a7e97daa
yangxuemin <18295894749@163.com>
2024-01-17 15:07:50 +0800

    修改

---
+++ ruoyi-model/src/main/java/com/ruoyi/shop/service/IShopInfoActivityService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.shop.service;
+
+import java.util.List;
+
+import com.ruoyi.shop.domain.ShopInfoActivity;
+
+/**
+ * 店铺信息活动配置Service接口
+ *
+ * @author ruoyi
+ * @date 2024-01-17
+ */
+public interface IShopInfoActivityService {
+    /**
+     * 查询店铺信息活动配置
+     *
+     * @param id 店铺信息活动配置主键
+     * @return 店铺信息活动配置
+     */
+    public ShopInfoActivity selectShopInfoActivityById(Long id);
+
+    /**
+     * 查询店铺信息活动配置列表
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 店铺信息活动配置集合
+     */
+    public List<ShopInfoActivity> selectShopInfoActivityList(ShopInfoActivity shopInfoActivity);
+
+    /**
+     * 新增店铺信息活动配置
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 结果
+     */
+    public int insertShopInfoActivity(ShopInfoActivity shopInfoActivity);
+
+    /**
+     * 修改店铺信息活动配置
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 结果
+     */
+    public int updateShopInfoActivity(ShopInfoActivity shopInfoActivity);
+
+    /**
+     * 批量删除店铺信息活动配置
+     *
+     * @param ids 需要删除的店铺信息活动配置主键集合
+     * @return 结果
+     */
+    public int deleteShopInfoActivityByIds(Long[] ids);
+
+    /**
+     * 删除店铺信息活动配置信息
+     *
+     * @param id 店铺信息活动配置主键
+     * @return 结果
+     */
+    public int deleteShopInfoActivityById(Long id);
+}
---
+++ ruoyi-model/src/main/java/com/ruoyi/shop/service/impl/ShopInfoActivityServiceImpl.java
@@ -0,0 +1,90 @@
+package com.ruoyi.shop.service.impl;
+
+import java.util.List;
+
+import com.ruoyi.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.shop.mapper.ShopInfoActivityMapper;
+import com.ruoyi.shop.domain.ShopInfoActivity;
+import com.ruoyi.shop.service.IShopInfoActivityService;
+
+/**
+ * 店铺信息活动配置Service业务层处理
+ *
+ * @author ruoyi
+ * @date 2024-01-17
+ */
+@Service
+public class ShopInfoActivityServiceImpl implements IShopInfoActivityService {
+    @Autowired
+    private ShopInfoActivityMapper shopInfoActivityMapper;
+
+    /**
+     * 查询店铺信息活动配置
+     *
+     * @param id 店铺信息活动配置主键
+     * @return 店铺信息活动配置
+     */
+    @Override
+    public ShopInfoActivity selectShopInfoActivityById(Long id) {
+        return shopInfoActivityMapper.selectShopInfoActivityById(id);
+    }
+
+    /**
+     * 查询店铺信息活动配置列表
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 店铺信息活动配置
+     */
+    @Override
+    public List<ShopInfoActivity> selectShopInfoActivityList(ShopInfoActivity shopInfoActivity) {
+        return shopInfoActivityMapper.selectShopInfoActivityList(shopInfoActivity);
+    }
+
+    /**
+     * 新增店铺信息活动配置
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 结果
+     */
+    @Override
+    public int insertShopInfoActivity(ShopInfoActivity shopInfoActivity) {
+        shopInfoActivity.setCreateTime(DateUtils.getNowDate());
+        return shopInfoActivityMapper.insertShopInfoActivity(shopInfoActivity);
+    }
+
+    /**
+     * 修改店铺信息活动配置
+     *
+     * @param shopInfoActivity 店铺信息活动配置
+     * @return 结果
+     */
+    @Override
+    public int updateShopInfoActivity(ShopInfoActivity shopInfoActivity) {
+        shopInfoActivity.setUpdateTime(DateUtils.getNowDate());
+        return shopInfoActivityMapper.updateShopInfoActivity(shopInfoActivity);
+    }
+
+    /**
+     * 批量删除店铺信息活动配置
+     *
+     * @param ids 需要删除的店铺信息活动配置主键
+     * @return 结果
+     */
+    @Override
+    public int deleteShopInfoActivityByIds(Long[] ids) {
+        return shopInfoActivityMapper.deleteShopInfoActivityByIds(ids);
+    }
+
+    /**
+     * 删除店铺信息活动配置信息
+     *
+     * @param id 店铺信息活动配置主键
+     * @return 结果
+     */
+    @Override
+    public int deleteShopInfoActivityById(Long id) {
+        return shopInfoActivityMapper.deleteShopInfoActivityById(id);
+    }
+}
