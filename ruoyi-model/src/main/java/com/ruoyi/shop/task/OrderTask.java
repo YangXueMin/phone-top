e6bc6702ce5aaff09864cf5f7bd7d13e21c6d100
yangxuemin <18295894749@163.com>
2024-01-25 10:56:08 +0800

    修改

--- ruoyi-model/src/main/java/com/ruoyi/shop/task/OrderTask.java
+++ ruoyi-model/src/main/java/com/ruoyi/shop/task/OrderTask.java
@@ -1,9 +1,18 @@
 package com.ruoyi.shop.task;
 
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.time.DateUtil;
+import com.ruoyi.shop.domain.Order;
+import com.ruoyi.shop.domain.RechargeOrderCoupon;
 import com.ruoyi.shop.mapper.OrderMapper;
+import com.ruoyi.shop.mapper.RechargeOrderCouponMapper;
 import com.ruoyi.shop.mapper.RechargeOrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author yangxuemin
@@ -17,9 +26,32 @@ public class OrderTask {
     private OrderMapper orderMapper;
     @Autowired
     private RechargeOrderMapper rechargeOrderMapper;
+    @Autowired
+    private RechargeOrderCouponMapper rechargeOrderCouponMapper;
 
     public void updateTimeOrderStatus() {
         System.out.println("更新订单状态");
+        Date date = new Date();
+        Order queryOrder = new Order();
+        queryOrder.setOrderStatus("1");
+        queryOrder.setPayType("2");
+        List<Order> orderList = orderMapper.selectOrderList(queryOrder);
+        if (orderList.size() > 0) {
+            for (Order order : orderList) {
+                if (order.getCreateTime().getTime() < DateUtil.subMinutes(date, 10).getTime()) {
+                    //还原用户优惠券
+                    String[] couponList = order.getCouponList().split(",");
+                    for (String couponId : couponList) {
+                        RechargeOrderCoupon rechargeOrderCoupon = rechargeOrderCouponMapper.selectRechargeOrderCouponById(Long.parseLong(couponId));
+                        if (rechargeOrderCoupon.getId() != null) {
+                            rechargeOrderCoupon.setStatus("1");
+                            rechargeOrderCoupon.setUpdateTime(DateUtils.getNowDate());
+                            rechargeOrderCouponMapper.updateRechargeOrderCoupon(rechargeOrderCoupon);
+                        }
+                    }
+                }
+            }
+        }
         orderMapper.updateTimeOrderStatus();
         rechargeOrderMapper.updateTimeOrderStatus();
     }
